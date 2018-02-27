e9c692453842f9b54c17b009daa04d2f8d09bab0
Zhang-O <691259192@qq.com>
2018-02-27 16:14:59 +0800

    knn

--- scratch/supervised_learning/kNN.py
+++ scratch/supervised_learning/kNN.py
@@ -15,14 +15,15 @@ class node:
         self.right = None
         self.point = point
         self.depth = depth
+        self.parent = None
 
 
 def median(array):
     """
-
     :param array:
     :return: the median pointer and its index
     """
+
     m = int(len(array) / 2)
     return array[m], m
 
@@ -31,31 +32,73 @@ def build_kd_tree(data, depth):
 
     dim = depth % data.shape[1]  # accoding which dim to split data
     data = data[data[:, dim].argsort()]  # sort the data according to dim
-    # data = sorted(data, key=lambda x: x[dim])
+    # data = sorted(data, key=lambda x: x[dim])  # not suitable to ndarray type
 
     # print(data)
     p, m = median(data)
     # print(p, m)
 
     tree = node(p, depth)
-
+    # print('m: {}; depth: {} parent: {}'.format(m, depth, tree.parent))
     if m > 0:
-        print('left: m: {}; depth: {}'.format(m, depth))
+
         tree.left = build_kd_tree(data[:m, :], depth+1)
+        tree.left.parent = tree
+        # print('left: m: {}; depth: {} parent: {}'.format(m, tree.left.depth, tree.left.parent))
+        # print('left: m: {}; depth: {} parent: {}'.format(m, depth, id(tree.parent)))
 
     if len(data) > 2:
-        print('right: m: {}; depth: {}'.format(m, depth))
+
         tree.right = build_kd_tree(data[m+1:, :], depth+1)
+        tree.right.parent = tree
+        # print('right: m: {}; depth: {} parent: {}'.format(m, tree.right.depth, tree.right.parent))
+        # print('right: m: {}; depth:{}  parent: {}'.format(m, depth, id(tree.parent)))
 
     return tree
 
 
 def distance(x, y, norm=2.0):
+    print(x,y)
     return pow(np.sum((x - y)**norm), 1 / norm)
     # return np.sqrt(np.sum((x - y)**2))
 
 
+def search_kd_tree(tree, d, target):
+    """
+
+    :param tree:
+    :param d:
+    :param target:
+    :return:
+    """
+    dim = tree.depth % len(target)
+    if target[dim] < tree.point[dim]:
+        if tree.left is not None:
+            return search_kd_tree(tree.left, d, target)
+    else:
+        if tree.right is not None:
+            return search_kd_tree(tree.right, d, target)
 
 
-result = build_kd_tree(T, 0)
+    def update_best(t, _best):
+        if t is None:
+            return
+        t = t.point
+        dis = distance(t, target)
+        if dis < _best[1]:
+            _best[1] = dis
+            _best[0] = t
+
+
+    best = [tree.point, 100000.0]
+    while tree.parent is not None:
+        update_best(tree.parent.left, best)
+        update_best(tree.parent.right, best)
+        tree = tree.parent
+    return best[0]
+
+
+
+kd_tree = build_kd_tree(T, 0)
+print(search_kd_tree(kd_tree, 0, [9, 4]))
 print('ok')
